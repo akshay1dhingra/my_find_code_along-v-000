bdec030275ef65cf41f27d3b26892537769854ac
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-11-06 01:04:03 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -8,5 +8,5 @@ def my_find(collection)
   end
 end
 
-collection = (1..100).to_a
+collection = (1..100).to_a #.to_a turns an object into an array
 my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
